4f4d38325b3cdd7247773751f83436cb5945a9cb
podhmo <ababjam61+github@gmail.com>
2017-09-13 19:37:55 +0900

    red test

---
+++ semver/tests/test_regression.py
@@ -0,0 +1,13 @@
+import pytest
+
+cands = [
+    # https://github.com/podhmo/python-semver/issues/5
+    ["<=1.2", "1.2.0", ["1.1.1", "1.2.0-pre", "1.2.0", "1.1.1-111", "1.1.1-21"]]
+]
+
+
+@pytest.mark.parametrize("op, wanted, cands", cands)
+def test_it(op, wanted, cands):
+    from semver import max_satisfying
+    got = max_satisfying(cands, op, loose=True)
+    assert got == "1.2.0"
