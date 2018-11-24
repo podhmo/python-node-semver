e06dfb688c066818a912920ee1f312bbc0ea2f20
podhmo <ababjam61+github@gmail.com>
2018-11-24 20:49:44 +0900

    bump version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PyTest(TestCommand):
 
 setup(
     name='node-semver',
-    version='0.5.1',
+    version='0.6.0',
     description='port of node-semver',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
